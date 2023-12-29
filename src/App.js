2267c5089e136e6cd15817dd7782294515654844
noora ha <noora.halla.aho@gmail.com>
2023-12-29 13:16:16 +0200

    add foundational components and data
    - add base components like section, card and sectiontitle
    - add list of work experience
    - add list of education history
    - clean up project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Section>
+        <Card>
+          <AboutMe />
+          <DownloadCVButton />
+        </Card>
+        <Card>
+          <AboutMe />
+        </Card>
+      </Section>
+      <Section>
+        <SectionTitle title="02. where I've worked" />
+      </Section>
     </div>
   );
+}
+
+function AboutMe() {
+  return (
+    <div className="About-Me">
+      <p>
+        I’m currently in my second year of studies at OSAO, but I’ve been
+        programming since I was 10 years old. I also have a passion for art and
+        design both 2D and 3D. I love to learn new things and have a vast range
+        of interests within the field of ICT, so I’m up for all kinds of work!{" "}
+      </p>
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <div className="Title">
+      <h4>hello! I'm</h4>
+      <h1>Noora Halla-aho</h1>
+      <h4>a software development student & artist</h4>
+    </div>
+  );
+}
+
+function SectionTitle({ title }) {
+  return (
+    <div className="Section-Title">
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+function Section({ children }) {
+  return <div className="Section">{children}</div>;
+}
+
+function Card({ children }) {
+  return <div className="Card">{children}</div>;
+}
+
+function DownloadCVButton() {
+  return <button className="Download-CV-Button">Download my CV</button>;
 }
 
 export default App;
